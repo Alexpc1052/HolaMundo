8f6c488062fa3d5755c024461f8bab4272891e0a
Alexpc1052 <acorreamonroy@gmail.com>
2022-08-14 14:55:09 -0500

    Se reemplaza Clase persona por registro Persona.

--- src/com/aleks/Persona.java
+++ src/com/aleks/Persona.java
@@ -4,5 +4,6 @@ public record Persona(String nombre, String apellidos) {
     @Override
     public String toString() {
         return "%s %s".formatted(nombre, apellidos);
+
     }
 }
--- src/com/aleks/PersonaRegistro.java
+++
@@ -1,9 +0,0 @@
-package com.aleks;
-
-public record PersonaRegistro(String nombre, String apellidos) {
-    @Override
-    public String toString() {
-        return "%s %s".formatted(nombre, apellidos);
-
-    }
-}
