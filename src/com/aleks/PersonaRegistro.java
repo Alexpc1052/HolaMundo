f90172181bdfa4ce7ac3c6fe2deee97f99338788
Alexpc1052 <acorreamonroy@gmail.com>
2022-08-14 14:40:30 -0500

    Se quitó la linea de código inservible.

--- src/com/aleks/PersonaRegistro.java
+++ src/com/aleks/PersonaRegistro.java
@@ -3,8 +3,6 @@ package com.aleks;
 public record PersonaRegistro(String nombre, String apellidos) {
     @Override
     public String toString() {
-        //Alexander comenté la liena de código que considero se puede eliminar
-        //System.out.println("esta instrucción es inservible");
         return "%s %s".formatted(nombre, apellidos);
 
     }
