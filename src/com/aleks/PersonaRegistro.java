71215ea29d0bb832e5e4c3c5145095cfe006371f
Alexpc1052 <acorreamonroy@gmail.com>
2022-08-14 14:15:08 -0500

    Se agregó una linea de código inservible.

--- src/com/aleks/PersonaRegistro.java
+++ src/com/aleks/PersonaRegistro.java
@@ -3,6 +3,7 @@ package com.aleks;
 public record PersonaRegistro(String nombre, String apellidos) {
     @Override
     public String toString() {
+        System.out.println("esta instrucción es inservible");
         return "%s %s".formatted(nombre, apellidos);
 
     }
