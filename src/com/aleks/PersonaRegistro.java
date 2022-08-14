d299a6e914ca2313ea82e2c0a8e40479462df62d
Alexpc1052 <104664378+Alexpc1052@users.noreply.github.com>
2022-08-14 14:29:32 -0500

    Comentario sobre código inservible

--- src/com/aleks/PersonaRegistro.java
+++ src/com/aleks/PersonaRegistro.java
@@ -3,7 +3,8 @@ package com.aleks;
 public record PersonaRegistro(String nombre, String apellidos) {
     @Override
     public String toString() {
-        System.out.println("esta instrucción es inservible");
+        //Alexander comenté la liena de código que considero se puede eliminar
+        //System.out.println("esta instrucción es inservible");
         return "%s %s".formatted(nombre, apellidos);
 
     }
