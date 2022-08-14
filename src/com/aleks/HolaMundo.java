b7b9741bea134db83fae306e7b39f315bacf07b8
Alexpc1052 <acorreamonroy@gmail.com>
2022-08-14 15:04:41 -0500

    Actualicé el mensaje de dialogo nombre a nombres en plural.

--- src/com/aleks/HolaMundo.java
+++ src/com/aleks/HolaMundo.java
@@ -3,7 +3,7 @@ package com.aleks;
 public class HolaMundo {
     public static void main(String[] args) {
         Mensaje msg = new Mensaje();
-        String nombre = msg.solicitaDatos("Escribe tu nombre: ");
+        String nombre = msg.solicitaDatos("Escribe tus nombres: ");
         String apellidos = msg.solicitaDatos("Escribe tus apellidos: ");
         Persona p1 = new Persona(nombre, apellidos);
         msg.saludar(p1);
